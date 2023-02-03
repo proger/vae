e54e38f0029826b9d614885db4e176ebb6bf6763
Volodymyr Kyrylov <vol@wilab.org.ua>
2023-02-03 20:39:29 +0100

    complete semhash protocol

--- scripts/train_semhash.py
+++ scripts/train_semhash.py
@@ -8,14 +8,16 @@ from va.newsgroups import make_datasets
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'mps')
+parser.add_argument('--latent_features', type=int, default=64, help='number of latent Bernoulli variables (bits)')
+parser.add_argument('--decoder_hidden_features', type=int, nargs='*', default=(), help='list of hidden layer dimensions for the decoder, one for each layer')
 args = parser.parse_args()
 
 torch.manual_seed(1)
 
 model = SematicHasher(
     vocab_size=10000,
-    latent_features=64,
-    decoder_hidden_features=()
+    latent_features=args.latent_features,
+    decoder_hidden_features=args.decoder_hidden_features
 )
 model.to(args.device)
 
@@ -23,6 +25,9 @@ print(model)
 
 optimizer = model.make_optimizer(lr=1e-3)
 train, test = make_datasets()
+
+print('train documents', len(train))
+print('test documents', len(test))
 
 train_loader = torch.utils.data.DataLoader(train, batch_size=256, shuffle=True, num_workers=0)
 test_loader = torch.utils.data.DataLoader(test, batch_size=256, shuffle=False, num_workers=0)
@@ -59,14 +64,31 @@ def evaluate(model, test_loader):
 
     return valid_loss
 
+train_losses = []
+test_losses = []
 
-for epoch in range(1500):
+for epoch in range(1000):
     train_loss = train(model, train_loader, optimizer)
     test_loss = evaluate(model, test_loader)
     print(f'Epoch {epoch}: train_loss={train_loss:.2f} test_loss={test_loss:.2f}')
+    train_losses.append(train_loss)
+    test_losses.append(test_loss)
 
+# 500 more epochs with 10 REINFORCE steps
+for epoch in range(1000, 1500):
+    train_loss = train(model, train_loader, optimizer, reinforce_steps=10)
+    test_loss = evaluate(model, test_loader)
+    print(f'Epoch {epoch}: train_loss={train_loss:.2f} test_loss={test_loss:.2f}')
+    train_losses.append(train_loss)
+    test_losses.append(test_loss)
+
+print('Best train loss', min(train_losses))
+print('Best test loss', min(test_losses))
 
 torch.save({
     'model': model.state_dict(),
     'optimizer': optimizer.state_dict(),
+    'train_losses': train_losses,
+    'test_losses': test_losses,
+    'args': vars(args),
 }, '20newsgroups.pt')
